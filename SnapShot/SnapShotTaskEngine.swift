ba7c46ed0d2ffd51dfb7273cabe191f75a12b3e6
zhendless <>
2015-12-11 08:57:53 +0800

    增加TaskEngine类

---
+++ SnapShot/SnapShotTaskEngine.swift
@@ -0,0 +1,23 @@
+//
+//  SnapShotTaskEngine.swift
+//  SnapShot
+//
+//  Created by 张磊 on 15/12/10.
+//  Copyright © 2015年 Jacob Li. All rights reserved.
+//
+
+import Foundation
+
+
+class SnapShotTaskEngine {
+    
+    class func getInstance() -> SnapShotTaskEngine! {
+        struct SnapShotSingleton {
+            static var predicate: dispatch_once_t = 0
+            static var instance: SnapShotTaskEngine? = nil
+        }
+        dispatch_once(&SnapShotSingleton.predicate, {SnapShotSingleton.instance = SnapShotTaskEngine()})
+        return SnapShotSingleton.instance
+    }
+    
+}
