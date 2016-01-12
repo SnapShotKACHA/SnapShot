6826c6c98c3890371dbc0b4743509230f7dde464
jacobTough <qqliteng@163.com>
2016-01-12 14:38:25 +0800

    import AMap framework

---
+++ SnapShot/LocationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LocationViewController.swift
+//  SnapShot
+//
+//  Created by Jacob Li on 12/01/2016.
+//  Copyright © 2016 Jacob Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LocationViewController: BasicViewController {
+    
+}
